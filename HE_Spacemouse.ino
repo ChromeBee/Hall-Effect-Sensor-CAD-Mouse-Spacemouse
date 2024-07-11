92669d66b050cd9b9b74468363e4af6645bfd8dd
ChromeBee <141455861+ChromeBee@users.noreply.github.com>
2024-07-11 17:28:45 +0100

    Add files via upload
    Wiring diagram and Arduino sketch

---
+++ HE_Spacemouse.ino
@@ -0,0 +1,340 @@
+// This code is the combination of multiple works by others:
+// 1. Original code for the Space Mushroom by Shiura on Thingiverse: https://www.thingiverse.com/thing:5739462
+//    The next two from the comments on the instructables page: https://www.instructables.com/Space-Mushroom-Full-6-DOFs-Controller-for-CAD-Appl/
+//    and the comments of Thingiverse: https://www.thingiverse.com/thing:5739462/comments
+// 2. Code to emulate a 3DConnexion Space Mouse by jfedor: https://pastebin.com/gQxUrScV
+// 3. This code was then remixed by BennyBWalker to include the above two sketches: https://pastebin.com/erhTgRBH
+// 4. Four joystick remix code by fdmakara: https://www.thingiverse.com/thing:5817728
+// The work by Teaching Tech involves mixing all of these. The basis is fdmakara's four joystick movement logic, with jfedor/BennyBWalker's HID SpaceMouse emulation.
+// The four joystick logic sketch was setup for the joystick library instead of HID, so elements of this were omitted where not needed.
+// The outputs were jumbled no matter how He plugged them in, so I spent a lot of time adding debugging code to track exactly what was happening.
+// On top of this, He has added more control of speed/direction and comments/links to informative resources to try and explain what is happening in each phase.
+
+// Spacemouse emulation
+// Teaching Tech followed the instructions here from nebhead: https://gist.github.com/nebhead/c92da8f1a8b476f7c36c032a0ac2592a
+// with two key differences:
+// 1. He changed the word 'DaemonBite' to 'Spacemouse' in all references. 2. He changed the VID and PID values as per jfedor's instructions: vid=0x256f, pid=0xc631 (SpaceMouse Pro Wireless (cabled))
+// When compiling and uploading, He select Arduino AVR boards (in Sketchbook) > Spacemouse and then the serial port.
+// You will also need to download and install the 3DConnexion software: https://3dconnexion.com/us/drivers-application/3dxware-10/
+// If all goes well, the 3DConnexion software will show a SpaceMouse Pro wireless when the Arduino is connected.
+// *JC - John Crombie - I'd like to add a big thank yoy to Teaching Tech for this code. If it wasn't for this I wouldn't
+// have been tempted to try to implement a Hall Effect sensor version. All my changes will be marked with a *JC comment
+// All debug Serial.print statements have had the joystick output text has been renamed as HESx e.g. AX is now HSE0
+
+
+// Include inbuilt Arduino HID library by NicoHood: https://github.com/NicoHood/HID 
+#include "HID.h"
+
+// Debugging
+// 0: Debugging off. Set to this once everything is working.
+// 1: *JC Output raw Hall Effect Sensor values. 176 - 1024 raw ADC 10-bit values. 5V ADC reference
+// 2: *JC Output centered Hall Effect Sensor values. Values should be approx -424 to +424, jitter around 0 at idle. ADC reference 2.56v
+// 3: *JC Output centered Hall Effect Sensor values. Filtered for deadzone. Approx -424+DEADZONE to +424-DEADZONE, locked to zero at idle. Also button values. ADC reference 2.56v
+// 4: Output translation and rotation values. Approx -500 to +500 depending on the parameter. *JC ADC reference 2.56v
+// 5: Output debug 3 and 4 side by side for direct cause and effect reference. *JC ADC reference 2.56v
+int debug = 0;
+
+// Direction
+// Modify the direction of translation/rotation depending on preference. This can also be done per application in the 3DConnexion software.
+// Switch between true/false as desired.
+bool invX = false; // pan left/right
+bool invY = false; // pan up/down
+bool invZ = false; // zoom in/out
+bool invRX = false; // Rotate around X axis (tilt front/back)
+bool invRY = false; // Rotate around Y axis (tilt left/right)
+bool invRZ = false; // Rotate around Z axis (twist left/right)
+
+// Speed
+// Modify to change sensitibity/speed. Default and maximum 100. Works like a percentage ie. 50 is half as fast as default. This can also be done per application in the 3DConnexion software.
+int16_t speed = 80;
+
+
+// Default Assembly when looking from above: *JC modified for Hall Effect Sensors (HES)
+//      7 6          Y+
+//       |           .
+// 8 9 --+--2 3 X-...Z+...X+
+//       |           .
+//      0 1          Y-
+//
+// Wiring. Matches the first eight analogue pins of the Arduino Pro Micro (atmega32u4)
+int PINLIST[8] = { // The positions of the reads *JC comments indicate which Hall Effect sensor is connected
+  A0, // HES 6 o'clock left
+  A1, // HES 6 o'clock right
+  A2, // HES 3 o'clock near
+  A3, // HES 3 o'clock far
+  A6, // HES 12 o'clock right
+  A7, // HES 12 o'clock left
+  A8, // HES 9 o'clock far
+  A9  // HES 9 o'clock near
+};
+
+// *JC added button list for digital inputs
+int BTNLIST[3] = { // Button pin list
+  0,
+  1,
+  2
+};
+
+// Deadzone to filter out unintended movements. 
+// Increase if the mouse has small movements when it should be idle or the mouse is too senstive to subtle movements.
+// Note that the 3d Connections also has its own deadzone processes
+int DEADZONE = 35;
+
+
+// This portion sets up the communication with the 3DConnexion software. The communication protocol is created here.
+// hidReportDescriptor webpage can be found here: https://eleccelerator.com/tutorial-about-usb-hid-report-descriptors/ 
+static const uint8_t _hidReportDescriptor[] PROGMEM = {
+  0x05, 0x01,           //  Usage Page (Generic Desktop)
+  0x09, 0x08,           //  0x08: Usage (Multi-Axis)
+  0xa1, 0x01,           //  Collection (Application)
+  0xa1, 0x00,           // Collection (Physical)
+  0x85, 0x01,           //  Report ID
+  0x16, 0x00, 0x80,     //logical minimum (-500)
+  0x26, 0xff, 0x7f,     //logical maximum (500)
+  0x36, 0x00, 0x80,     //Physical Minimum (-32768)
+  0x46, 0xff, 0x7f,     //Physical Maximum (32767)
+  0x09, 0x30,           //    Usage (X)
+  0x09, 0x31,           //    Usage (Y)
+  0x09, 0x32,           //    Usage (Z)
+  0x75, 0x10,           //    Report Size (16)
+  0x95, 0x03,           //    Report Count (3)
+  0x81, 0x02,           //    Input (variable,absolute)
+  0xC0,                 //  End Collection
+  0xa1, 0x00,           // Collection (Physical)
+  0x85, 0x02,           //  Report ID
+  0x16, 0x00, 0x80,     //logical minimum (-500)
+  0x26, 0xff, 0x7f,     //logical maximum (500)
+  0x36, 0x00, 0x80,     //Physical Minimum (-32768)
+  0x46, 0xff, 0x7f,     //Physical Maximum (32767)
+  0x09, 0x33,           //    Usage (RX)
+  0x09, 0x34,           //    Usage (RY)
+  0x09, 0x35,           //    Usage (RZ)
+  0x75, 0x10,           //    Report Size (16)
+  0x95, 0x03,           //    Report Count (3)
+  0x81, 0x02,           //    Input (variable,absolute)
+  0xC0,                 //  End Collection
+ 
+  0xa1, 0x00,           // Collection (Physical)
+  0x85, 0x03,           //  Report ID
+  0x15, 0x00,           //   Logical Minimum (0)
+  0x25, 0x01,           //    Logical Maximum (1)
+  0x75, 0x01,           //    Report Size (1)
+  0x95, 32,             //    Report Count (24) // *JC - I dont undwerstand what the comment says 24 but gives a value of 32
+  0x05, 0x09,           //    Usage Page (Button)
+  0x19, 1,              //    Usage Minimum (Button #1)
+  0x29, 32,             //    Usage Maximum (Button #24) // *JC - same comment as above
+  0x81, 0x02,           //    Input (variable,absolute)
+  0xC0,
+  0xC0
+};
+
+// Sensors are matched to pin order.
+// *JC - Note HES0 and BTN0 are not the same pin. HSE0 is Analog input 0 and BTN0 is digital input 0
+#define HES0 0
+#define HES1 1
+#define HES2 2
+#define HES3 3
+#define HES6 4
+#define HES7 5
+#define HES8 6
+#define HES9 7
+#define BTN0 0
+#define BTN1 1
+#define BTN2 2
+
+// Centerpoint variable to be populated during setup routine.
+int centerPoints[8];
+
+// Function to read and store analogue voltages for each joystick axis.
+void readAllFromSensors(int *rawReads){
+  for(int i=0; i<8; i++){
+    rawReads[i] = analogRead(PINLIST[i]);
+  }
+}
+
+// *JC Function to read and store button values
+void readAllFromButtons(uint8_t *buttonValues){
+  for(int i=1; i<4; i++){ // read real button values
+    buttonValues[i] = !digitalRead(BTNLIST[i-1]);
+  }
+  //  based on real values set logical switch
+  if( buttonValues[1] && buttonValues[3]) {
+    buttonValues[0] = true;
+    buttonValues[1] = buttonValues[3] = false;
+  }
+  else
+  {
+    buttonValues[0] = false;
+  }
+}
+
+void setup() {
+  // HID protocol is set.
+  static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
+  HID().AppendDescriptor(&node);
+  // Begin Seral for debugging
+  Serial.begin(250000);
+  delay(100);
+  // *JC - setup button pins for digitalRead
+  for(int i=0; i<3; i++){
+    pinMode(BTNLIST[i],INPUT_PULLUP);
+  }
+  //*JC - reduce ADC reference voltage from 5V to 2.56 if not using debug = 1
+  if (debug != 1) {
+    analogReference(INTERNAL);
+  }
+
+  // Read idle/centre positions for Sensors.
+  // *JC - First read gives unpredictable values so do it twice
+  readAllFromSensors(centerPoints);
+  readAllFromSensors(centerPoints);
+}
+
+// Function to send translation and rotation data to the 3DConnexion software using the HID protocol outlined earlier. Two sets of data are sent: translation and then rotation.
+// For each, a 16bit integer is split into two using bit shifting. The first is mangitude and the second is direction.
+// *JC - Added button report
+void send_command(int16_t rx, int16_t ry, int16_t rz, int16_t x, int16_t y, int16_t z, uint8_t *buttonValues) {
+  uint8_t trans[6] = { x & 0xFF, x >> 8, y & 0xFF, y >> 8, z & 0xFF, z >> 8 };
+  HID().SendReport(1, trans, 6);
+  uint8_t rot[6] = { rx & 0xFF, rx >> 8, ry & 0xFF, ry >> 8, rz & 0xFF, rz >> 8 };
+  HID().SendReport(2, rot, 6);
+  // *JC - Button Report
+  // these are the button functions for first byte in Fusion 360
+  //  bit 0 - bring up configuration dialog - logical button (press BTN0 and BTN2 at the same time)
+  //  bit 1 - fit to screen
+  //  bit 2 - plan view
+  //  bit 3 - no function?
+  //  bit 4 - right view
+  //  bit 5 - front view
+  //  bit 6 - no function?
+  //  bit 7 - no function?
+  uint8_t btn[4] ={32*buttonValues[3]+16*buttonValues[2]+4*buttonValues[1]+buttonValues[0],0,0,0};
+  HID().SendReport(3,btn,4);
+}
+
+void loop() {
+  int rawReads[8], centered[8];
+  uint8_t buttonReads[4];
+  // sensor values are read. range should be 176 - 1024 for debug levels other than 1 and 88-860 for debug 1
+  readAllFromSensors(rawReads);
+  // button values true or false
+  readAllFromButtons(buttonReads);
+  // Report back 0-1023 raw ADC 10-bit values if enabled
+  if(debug == 1){ 
+    Serial.print("HES0:"); Serial.print(rawReads[0]); Serial.print(",");
+    Serial.print("HES1:"); Serial.print(rawReads[1]); Serial.print(",");
+    Serial.print("HES2:"); Serial.print(rawReads[2]); Serial.print(",");
+    Serial.print("HES3:"); Serial.print(rawReads[3]); Serial.print(",");
+    Serial.print("HES6:"); Serial.print(rawReads[4]); Serial.print(",");
+    Serial.print("HES7:"); Serial.print(rawReads[5]); Serial.print(",");
+    Serial.print("HES8:"); Serial.print(rawReads[6]); Serial.print(",");
+    Serial.print("HES9:"); Serial.println(rawReads[7]);
+  }
+
+  // Subtract centre position from measured position to determine movement.
+  // *JC - As we are going negative with the readings, we make them positive
+  // by subtraction them from the recorded centerPoints rather than the other was around.
+  for(int i=0; i<8; i++) centered[i] = centerPoints[i] - rawReads[i]; // 
+  // Report centered Sensor values if enabled. Values should be approx -256 to +256, jitter around 0 at idle.
+  if(debug == 2){
+    Serial.print("HES0:"); Serial.print(centered[0]); Serial.print(",");
+    Serial.print("HES1:"); Serial.print(centered[1]); Serial.print(",");
+    Serial.print("HES2:"); Serial.print(centered[2]); Serial.print(",");
+    Serial.print("HES3:"); Serial.print(centered[3]); Serial.print(",");
+    Serial.print("HES6:"); Serial.print(centered[4]); Serial.print(",");
+    Serial.print("HES7:"); Serial.print(centered[5]); Serial.print(",");
+    Serial.print("HES8:"); Serial.print(centered[6]); Serial.print(",");
+    Serial.print("HES9:"); Serial.println(centered[7]);
+  }
+  // Filter movement values. Set to zero if movement is below deadzone threshold.
+  // *JC - Changed operation so there isn't a sudden jump when the value first falls outside deadzone
+  for(int i=0; i<8; i++){
+    if(centered[i]<DEADZONE && centered[i]>-DEADZONE) {
+      centered[i] = 0;
+    } else {
+      int sgn = centered[i] / abs(centered[i]);
+      centered[i] = sgn*(abs(centered[i])-DEADZONE);
+    }
+  }
+  // Report centered Sensor values. Filtered for deadzone. Approx -500 to +500, locked to zero at idle
+  if(debug == 3){
+    Serial.print("HES0:"); Serial.print(centered[0]); Serial.print(",");
+    Serial.print("HES1:"); Serial.print(centered[1]); Serial.print(",");
+    Serial.print("HES2:"); Serial.print(centered[2]); Serial.print(",");
+    Serial.print("HES3:"); Serial.print(centered[3]); Serial.print(",");
+    Serial.print("HES6:"); Serial.print(centered[4]); Serial.print(",");
+    Serial.print("HES7:"); Serial.print(centered[5]); Serial.print(",");
+    Serial.print("HES8:"); Serial.print(centered[6]); Serial.print(",");
+    Serial.print("HES9:"); Serial.print(centered[7]); Serial.print(",");
+    Serial.print("But0:"); Serial.print(buttonReads[0]); Serial.print(",");
+    Serial.print("But1:"); Serial.print(buttonReads[1]); Serial.print(",");
+    Serial.print("But2:"); Serial.print(buttonReads[2]); Serial.print(",");
+    Serial.print("But3:"); Serial.println(buttonReads[3]);  
+  }
+
+  // Doing all through arithmetic contribution by fdmakara
+  // Integer has been changed to 16 bit int16_t to match what the HID protocol expects.
+  int16_t transX, transY, transZ, rotX, rotY, rotZ; // Declare movement variables at 16 bit integers
+  // Original fdmakara calculations
+  //transX = (-centered[AX] +centered[CX])/1;
+  //transY = (-centered[BX] +centered[DX])/1;
+  //transZ = (-centered[AY] -centered[BY] -centered[CY] -centered[DY])/2;
+  //rotX = (-centered[AY] +centered[CY])/2;
+  //rotY = (+centered[BY] -centered[DY])/2;
+  //rotZ = (+centered[AX] +centered[BX] +centered[CX] +centered[DX])/4;
+  
+  // *JC - Replaced Joystick calculations with ones for the Hall Effect Sensors
+  transX = (centered[HES1]-centered[HES0]+centered[HES6]-centered[HES7])/2;  
+  transY = (centered[HES2]-centered[HES3]+centered[HES9]-centered[HES8])/2;  
+  transZ = (centered[HES1]+centered[HES0]+centered[HES3]+centered[HES2]+centered[HES7]+centered[HES6]+centered[HES9]+centered[HES8])/4;
+  rotX = (centered[HES0]+centered[HES1]-centered[HES6]-centered[HES7])/2;
+  rotY = (centered[HES8]+centered[HES9]-centered[HES2]-centered[HES3])/2;
+  rotZ = (centered[HES1]+centered[HES3]+centered[HES7]+centered[HES9]-centered[HES0]-centered[HES2]-centered[HES6]-centered[HES8])/4;
+// *JC - modified speed calculation to allow for the fact that this is integer calculations
+// so do multiplications prior to divisions to maintain maximum accuracy.
+  transX = (transX*speed)/100;
+  transY = (transY*speed)/100;
+  transZ = (transZ*speed)/100;
+  rotX = (rotX*speed)/100;
+  rotY = (rotY*speed)/100;
+  rotZ = (rotZ*speed)/100;
+// Invert directions if needed
+  if(invX == true){ transX = transX*-1;};
+  if(invY == true){ transY = transY*-1;};
+  if(invZ == true){ transZ = transZ*-1;};
+  if(invRX == true){ rotX = rotX*-1;};
+  if(invRY == true){ rotY = rotY*-1;};
+  if(invRZ == true){ rotZ = rotZ*-1;};
+
+// Report translation and rotation values if enabled. Approx -800 to 800 depending on the parameter.
+  if(debug == 4){
+    Serial.print("TX:"); Serial.print(transX); Serial.print(",");
+    Serial.print("TY:"); Serial.print(transY); Serial.print(",");
+    Serial.print("TZ:"); Serial.print(transZ); Serial.print(",");
+    Serial.print("RX:"); Serial.print(rotX); Serial.print(",");
+    Serial.print("RY:"); Serial.print(rotY); Serial.print(",");
+    Serial.print("RZ:"); Serial.println(rotZ);
+  }
+// Report debug 4 and 5 info side by side for direct reference if enabled. Very useful if you need to alter which inputs are used in th arithmatic above.
+  if(debug == 5){
+    Serial.print("HES0:"); Serial.print(centered[0]); Serial.print(",");
+    Serial.print("HES1:"); Serial.print(centered[1]); Serial.print(",");
+    Serial.print("HES2:"); Serial.print(centered[2]); Serial.print(",");
+    Serial.print("HES3:"); Serial.print(centered[3]); Serial.print(",");
+    Serial.print("HES6:"); Serial.print(centered[4]); Serial.print(",");
+    Serial.print("HES7:"); Serial.print(centered[5]); Serial.print(",");
+    Serial.print("HES8:"); Serial.print(centered[6]); Serial.print(",");
+    Serial.print("HES9:"); Serial.print(centered[7]); Serial.print("||");
+    Serial.print("TX:"); Serial.print(transX); Serial.print(",");
+    Serial.print("TY:"); Serial.print(transY); Serial.print(",");
+    Serial.print("TZ:"); Serial.print(transZ); Serial.print(",");
+    Serial.print("RX:"); Serial.print(rotX); Serial.print(",");
+    Serial.print("RY:"); Serial.print(rotY); Serial.print(",");
+    Serial.print("RZ:"); Serial.println(rotZ);
+  }
+
+// Send data to the 3DConnexion software.
+// The correct order for me was determined after trial and error by Teaching Tech
+// *JC - Added buttons for button report
+  send_command(rotX, rotY, rotZ, transX, transZ, transY,buttonReads);
+}
